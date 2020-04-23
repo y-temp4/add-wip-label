ddb3dcf9255b906ae87a82c26eca218dabb2caff
y-temp4 <y.temp4@gmail.com>
2020-04-23 12:54:30 +0900

    Add github action

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,22 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const prTitle = getPrTitle()
+    console.log({ prTitle })
   } catch (error) {
     core.setFailed(error.message)
   }
+}
+
+function getPrTitle() {
+  const pullRequest = github.context.payload.pull_request
+  if (!pullRequest) {
+    return undefined
+  }
+
+  return pullRequest.body
 }
 
 run()
